a58853913547f8c0e635b1a86dd3eed6d5ee54c6
jaikumar555 <75935841+jaikumar555@users.noreply.github.com>
2020-12-13 18:19:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import MindNode from './components/node.component'
+
+function App() {
+  return (
+    <div className="App">
+     <MindNode /> 
+    </div>
+  );
+}
+
+export default App;
